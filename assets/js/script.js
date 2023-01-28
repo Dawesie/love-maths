cae88f8170872ad48a42eb2c345c611d242f004a
Dawesie <marie.dawe01967@gmail.com>
2023-01-28 14:50:01 +0000

    Add inital structure to set up structures for game function

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+
+
+
+function runGame() {
+
+}
